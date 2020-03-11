431373b9da8180bbf94e6ed053352b56d5c60949
hly22 <54061984+hly22@users.noreply.github.com>
2020-03-12 10:45:56 +1100

    3 hours 30 min

--- Assets/Scripts/PlayerScripts/PlayerFootsteps.cs
+++ Assets/Scripts/PlayerScripts/PlayerFootsteps.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerFootsteps : MonoBehaviour {
+public class PlayerFootsteps : MonoBehaviour
+{
 
     private AudioSource footstep_Sound;
 
@@ -19,28 +20,54 @@ public class PlayerFootsteps : MonoBehaviour {
     [HideInInspector]
     public float step_Distance;
 
-    // Start is called before the first frame update
-    void Awake ()  {
+    void Awake()
+    {
         footstep_Sound = GetComponent<AudioSource>();
 
-        character_Controller = GetComponent<CharacterController>();
+        character_Controller = GetComponentInParent<CharacterController>();
     }
 
-    
-    void Update(){
+    void Update()
+    {
         CheckToPlayFootstepSound();
     }
 
-    void CheckToPlayFootstepSound() {
+    void CheckToPlayFootstepSound()
+    {
 
+        // if we are NOT on the ground
         if (!character_Controller.isGrounded)
             return;
 
-        if(character_Controller.velocity.sqrMagnitude > 0) {
 
+        if (character_Controller.velocity.sqrMagnitude > 0)
+        {
+
+            // accumulated distance is the value how far can we go 
+            // e.g. make a step or sprint, or move while crouching
+            // until we play the footstep sound
             accumulated_Distance += Time.deltaTime;
 
+            if (accumulated_Distance > step_Distance)
+            {
+
+                footstep_Sound.volume = Random.Range(volume_Min, volume_Max);
+                footstep_Sound.clip = footstep_Clip[Random.Range(0, footstep_Clip.Length)];
+                footstep_Sound.Play();
+
+                accumulated_Distance = 0f;
+
+            }
+
         }
+        else
+        {
+            accumulated_Distance = 0f;
+        }
+
+
     }
 
-} //class
+
+
+} // class
