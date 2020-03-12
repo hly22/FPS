98941a30ad639d1acad319f41b95ef09a25c98b3
hly22 <54061984+hly22@users.noreply.github.com>
2020-03-12 13:09:10 +1100

    3:50

--- Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerAttack : MonoBehaviour {
+public class PlayerAttack : MonoBehaviour
+{
 
     private WeaponManager weapon_Manager;
 
@@ -11,32 +12,51 @@ public class PlayerAttack : MonoBehaviour {
     private float nextTimeToFire;
     public float damage = 20f;
 
-    void Awake() {
+    private Animator zoomCameraAnim;
+    private bool zoomed;
+
+    private Camera mainCam;
+
+    private GameObject crosshair;
+
+    void Awake()
+    {
 
         weapon_Manager = GetComponent<WeaponManager>();
+
+        zoomCameraAnim = transform.Find(Tags.LOOK_ROOT)
+        .transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
+
+        crosshair = GameObject.FindWithTag(Tags.CROSSHAIR);
+
+        mainCam = Camera.main;
 
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()  {
+    void Update()
+    {
         WeaponShoot();
 
     }
 
-    void WeaponShoot() {
+    void WeaponShoot()
+    {
 
         // if we have assualt rifle
-        if(weapon_Manager.GetCurrentSelectedWeapon().fireType == WeaponFireType.MULTIPLE) {
+        if (weapon_Manager.GetCurrentSelectedWeapon().fireType == WeaponFireType.MULTIPLE)
+        {
 
             // if we press and hold left mouse click and
             // if time is greater than the nextTimeToFire
-            if(Input.GetMouseButton(0) && Time.time > nextTimeToFire) {
+            if (Input.GetMouseButton(0) && Time.time > nextTimeToFire)
+            {
 
                 nextTimeToFire = Time.time + 1f / fireRate;
 
@@ -46,23 +66,30 @@ public class PlayerAttack : MonoBehaviour {
 
             }
             // if we have a regular weapon that shoots once
-        } else {
+        }
+        else
+        {
 
-            if(Input.GetMouseButtonDown(0)) {
+            if (Input.GetMouseButtonDown(0))
+            {
 
                 // handle axe
-                if(weapon_Manager.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG) {
+                if (weapon_Manager.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG)
+                {
                     weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
                 }
 
                 // handle shoot
-                if (weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET) 
+                if (weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
+                {
 
                     weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
 
                     // BulletFired():
 
-                } else {
+                }
+                else
+                {
                     // we have an arrow or spear
 
                 } // else
@@ -71,5 +98,13 @@ public class PlayerAttack : MonoBehaviour {
 
         } // else
 
+    } // weapon shoot
+
+    void ZoomInAndOut() {
+
+    }
+
+
 
 } // class
+
